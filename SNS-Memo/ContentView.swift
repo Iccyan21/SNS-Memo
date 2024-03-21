d21125830d4def1086d5728a99bdc5d6529c0175
Iccyan21 <iccyan0429@gmail.com>
2024-03-21 13:58:50 +0900

    first commit

---
+++ SNS-Memo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SNS-Memo
+//
+//  Created by 水原　樹 on 2024/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SNS-Memo/SNS_MemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SNS_MemoApp.swift
+//  SNS-Memo
+//
+//  Created by 水原　樹 on 2024/03/21.
+//
+
+import SwiftUI
+
+@main
+struct SNS_MemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
