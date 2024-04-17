17d11fc5d4d6c3ff07c44004b2aff00bce83c7a5
Iccyan21 <iccyan0429@gmail.com>
2024-04-17 15:37:27 +0900

    SettiingView

--- SNS-Memo/SettingView.swift
+++ SNS-Memo/SettingView.swift
@@ -8,18 +8,63 @@
 import SwiftUI
 
 struct SettingView: View {
+    @Environment(\.openURL) var openURL
+    
     var body: some View {
+        
         VStack{
             ZStack{
                 Color.orange.edgesIgnoringSafeArea(.all)
-                Text("設定")
-                    .font(.largeTitle)
-                    .foregroundColor(.white)
-                    .background(Color.orange)
-                    .padding()
+                
             }.frame(height: 50)
+            
             Spacer()
-        }
+            
+            VStack{
+                List{
+                    NavigationLink(destination: Text("広告削除")) {
+                        Text("広告削除")
+                            .foregroundColor(.black)
+                    }
+                    
+                    Button(action: {
+                        // ここにメーラーを起動するコードを書く
+                        let email = "ashitagogo123@gmail.com" // 送信先のメールアドレス
+                        let subject = "ご意見・ご要望" // メールの件名
+                        let body = "ご意見・ご要望など、お気軽にお寄せください。" // メールの本文
+                        let encodedSubject = subject.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
+                        let encodedBody = body.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
+                        let mailtoURL = URL(string: "mailto:\(email)?subject=\(encodedSubject)&body=\(encodedBody)")!
+                        openURL(mailtoURL)
+                    }, label: {
+                        Text("ご意見・ご要望など")
+                            .foregroundColor(.black)
+                    })
+                    HStack{
+                        NavigationLink(destination: Text("レビューを書く")) {
+                            Text("レビューを書く")
+                                .foregroundColor(.black)
+                            
+                            Spacer()
+                            
+                            
+                        }
+                    }
+                    
+                    NavigationLink(destination: TermsView()) {
+                        Text("利用規約")
+                            .foregroundColor(.black)
+              
+                    }
+                    NavigationLink(
+                        destination: PrivacyPolicyView(),
+                        label: {
+                            Text("プライバシーポリシー").foregroundColor(.black)
+                        })
+                }
+                
+            }
+        }.navigationTitle("設定")
     }
 }
 
---
+++ SNS-Memo/TermsView.swift
@@ -0,0 +1,18 @@
+//
+//  TermsView.swift
+//  SNS-Memo
+//
+//  Created by 水原　樹 on 2024/04/16.
+//
+
+import SwiftUI
+
+struct TermsView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    TermsView()
+}
